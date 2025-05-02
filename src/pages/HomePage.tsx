686557ece4d9c27cad16f21c84c3859b9bd286ac
김민선 <minsun9856@gmail.com>
2025-05-02 15:54:19 +0900

    design: 커뮤니티 리스트(웹) UI

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,6 +4,8 @@ import bannerImg1 from "../assets/card1.jpg";
 import bannerImg2 from "../assets/card2.jpg";
 import bannerImg3 from "../assets/card3.jpg";
 import bannerImg4 from "../assets/card4.jpg";
+import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
+import { TfiCommentAlt } from "react-icons/tfi";
 
 const bannerList = [
   {
@@ -32,10 +34,34 @@ const bannerList = [
   },
 ];
 
+const posts = [
+  {
+    id: "1",
+    createdAt: "25.05.02",
+    title: "첫 번째 게시글입니다.",
+    viewCount: 123,
+    commentCount: 2,
+  },
+  {
+    id: "2",
+    createdAt: "25.04.30",
+    title: "두 번째 글",
+    viewCount: 77,
+    commentCount: 0,
+  },
+  {
+    id: "3",
+    createdAt: "25.04.28",
+    title: "세 번째 글입니다.",
+    viewCount: 45,
+    commentCount: 0,
+  },
+];
+
 export default function HomePage() {
   return (
     <DefaultLayout>
-      <div className="font-pretendard md:pt-[100px] w-full">
+      <div className="font-pretendard md:py-[100px] w-full">
         <div className="text-center text-[16px] md:text-[18px] text-[#5E616E] font-bold mb-2">
           다글제작소
         </div>
@@ -45,7 +71,7 @@ export default function HomePage() {
           오신 것을 환영합니다.
         </div>
 
-        <div className="overflow-hidden group cursor-pointer">
+        <div className="overflow-hidden group cursor-pointer mb-[40px]">
           <div className="flex w-max animate-scrollX pause-on-hover whitespace-nowrap">
             {[...bannerList, ...bannerList].map((item, idx) => (
               <div key={idx} className="inline-block mx-2">
@@ -57,6 +83,62 @@ export default function HomePage() {
                 />
               </div>
             ))}
+          </div>
+        </div>
+
+        <div className="bg-white rounded-[12px]">
+          <div className="flex flex-row items-center justify-between mb-4 pt-[24px] px-[24px]">
+            <div className="font-bold text-[24px]">게시판</div>
+            <button
+              style={{ backgroundColor: "#6025E1" }}
+              className="text-white px-4 py-2.5 rounded-[8px] font-bold"
+            >
+              글쓰기
+            </button>
+          </div>
+          <table className="w-full table-fixed border-t border-b border-gray-200 text-left text-[18px]">
+            <colgroup>
+              <col style={{ width: "80%" }} />
+              <col style={{ width: "15%" }} />
+              <col style={{ width: "5%" }} />
+            </colgroup>
+            <tbody>
+              {posts.map((post) => (
+                <tr key={post.id} className="cursor-pointer">
+                  <td className="py-3.5 px-[24px] border-b w-[80%] text-[18px]">
+                    <div className="truncate whitespace-nowrap overflow-hidden">
+                      {post.title}
+                    </div>
+                  </td>
+                  <td className="py-3.5 px-[24px] border-b w-[10%] text-[16px] text-gray-500">
+                    {post.createdAt}
+                  </td>
+                  <td className="py-3.5 px-[24px] border-b w-[10%] text-[16px] text-gray-500">
+                    <div className="flex flex-row items-center gap-1">
+                      <TfiCommentAlt />
+                      {post.commentCount}
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+
+          <div className="mt-6 flex justify-center items-center space-x-2 text-sm pb-[24px] px-[24px]">
+            <button
+              className="p-2 text-gray-600 hover:text-black disabled:text-gray-300"
+              disabled
+            >
+              <HiChevronLeft size={20} />
+            </button>
+
+            <button className="px-3 py-1 rounded hover:bg-gray-100">1</button>
+            <button className="px-3 py-1 rounded hover:bg-gray-100">2</button>
+            <button className="px-3 py-1 rounded hover:bg-gray-100">3</button>
+
+            <button className="p-2 text-gray-600 hover:text-black">
+              <HiChevronRight size={20} />
+            </button>
           </div>
         </div>
       </div>
